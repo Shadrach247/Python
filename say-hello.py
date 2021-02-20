86fff69c74c9faaf90bdcfc09e0fcaba1b33d30a
shadrach247 <soshadrach@gmail.com>
2021-02-20 14:14:11 +0100

    Added say-hello and str-function

---
+++ say-hello.py
@@ -0,0 +1,35 @@
+# This program says hello and asks for any name.
+print('Hello world!')
+
+# Ask for your name
+print('What is you name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+
+# Ask for your age
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ str-function.py
@@ -0,0 +1,8 @@
+print( str(35) )
+
+print('I am ' + str(35) + ' years old')
+
+# input always return a string
+spam = int(input())
+print(spam)
+print(spam * 10 / 5)
