3f821e89caa93da4f39774207c3c0b08fc4a5ad8
shadrach247 <soshadrach@gmail.com>
2021-03-09 13:35:54 +0100

    Added more functions contents

---
+++ functions/sameName2.py
@@ -0,0 +1,22 @@
+def spam():
+    global eggs
+    eggs = 'spam'
+
+eggs = 'global'
+spam()
+print(eggs)
+
+""" 
+    There are four rules to tell whether a variable is in a local scope or
+global scope:
+    If a variable is being used in the global scope (that is, outside of all
+functions), then it is always a global variable.
+    If there is a global statement for that variable in a function, it is a global
+variable.
+    Otherwise, if the variable is used in an assignment statement in the
+function, it is a local variable.
+    But if the variable is not used in an assignment statement, it is a global
+variable.
+
+"""
+
